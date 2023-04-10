81de2c61fd28859b96e859cd647438e101b0d601
alexanderlau203 <45554266+alexanderlau203@users.noreply.github.com>
2023-04-10 22:18:14 +1000

    Removed google analytics

--- src/App.js
+++ src/App.js
@@ -1,26 +1,12 @@
-import React from 'react';
-import { 
-  Routes,
-  Route,
-  useLocation
-} from "react-router-dom";
-import Home from './pages/Home';
-import About from './pages/About';
-import Contact from './pages/Contact';
-import Compositions from './pages/Compositions';
-import CompositionDetails from './pages/CompositionDetails';
-import ReactGA from 'react-ga4';
-
-ReactGA.initialize(process.env.REACT_APP_GOOGLE_ANALYTICS_ID)
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Compositions from "./pages/Compositions";
+import CompositionDetails from "./pages/CompositionDetails";
 
 function App() {
-  const location = useLocation()
-
-  React.useEffect(() => {
-    ReactGA.send({ hitType: "pageview", page: location.pathname })
-    console.log(location.pathname)
-  }, [location])
-
   return (
     <Routes>
       <Route path="/" element={<Home />} />
